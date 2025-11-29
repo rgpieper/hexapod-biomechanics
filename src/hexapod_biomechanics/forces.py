fix: Correct unit disagreement in moment calculation and specify units throughout

src/hexapod_biomechanics/forces.py
@@ -19,11 +19,11 @@ class HexKistler:
 
         self.cluster_hex_base = np.nanmean(cluster_hex_static, axis=0) # (n_markers, 3)
 
-        self.a_z0 = -41 # mm, distance from force plate surface to sensor plane / Kistler origin
-        self.a = 210 # mm, distance from sensor axis to kistler y-axis
-        self.b = 260 # mm, distance from sensor axis to Kistler x-axis
+        self.a_z0 = -41 # [mm], distance from force plate surface to sensor plane / Kistler origin
+        self.a = 210 # [mm], distance from sensor axis to kistler y-axis
+        self.b = 260 # [mm], distance from sensor axis to Kistler x-axis
 
-        h_marker = 12 # mm, marker height (distance from marker to force plate surface)
+        h_marker = 12 # [mm], marker height (distance from marker to force plate surface)
 
         distances = pdist(self.cluster_hex_base)
         pairs = list(combinations(range(self.cluster_hex_base.shape[0]), 2))
@@ -53,24 +53,24 @@ class HexKistler:
         """Convert raw forces to ground reaction force parameters, accounting for transformation of the Hexapod.
 
         Args:
-            cluster_hex (npt.NDArray): Hexapod marker trajectories (n_frames,n_markers,3)
-            fx12 (npt.NDArray): Raw x-direction force acting on Kistler between sensors 1 and 2 (n_frames,)
-            fx34 (npt.NDArray): Raw x-direction force acting on Kistler between sensros 3 and 4 (n_frames,)
-            fy14 (npt.NDArray): Raw y-direction force acting on Kistler between sensors 1 and 4 (n_frames,)
-            fy23 (npt.NDArray): Raw y-direction force acting on Kistler between sensors 2 and 3 (n_frames,)
-            fz1 (npt.NDArray): Raw z-direction force acting on Kistler at sensor 1 (n_frames,)
-            fz2 (npt.NDArray): Raw z-direction force acting on Kistler at sensor 2 (n_frames,)
-            fz3 (npt.NDArray): Raw z-direction force acting on Kistler at sensor 3 (n_frames,)
-            fz4 (npt.NDArray): Raw z-direction force acting on Kistler at sensor 4 (n_frames,)
-            stance_thresh (float, optional): Vertical (z) force at which subject is in stance on Hexapod, when COP will be computed. Defaults to 18.0.
+            cluster_hex (npt.NDArray): Hexapod marker trajectories [mm] (n_frames,n_markers,3)
+            fx12 (npt.NDArray): Raw x-direction force acting on Kistler between sensors 1 and 2 [N] (n_frames,)
+            fx34 (npt.NDArray): Raw x-direction force acting on Kistler between sensros 3 and 4 [N] (n_frames,)
+            fy14 (npt.NDArray): Raw y-direction force acting on Kistler between sensors 1 and 4 [N] (n_frames,)
+            fy23 (npt.NDArray): Raw y-direction force acting on Kistler between sensors 2 and 3 [N] (n_frames,)
+            fz1 (npt.NDArray): Raw z-direction force acting on Kistler at sensor 1 [N] (n_frames,)
+            fz2 (npt.NDArray): Raw z-direction force acting on Kistler at sensor 2 [N] (n_frames,)
+            fz3 (npt.NDArray): Raw z-direction force acting on Kistler at sensor 3 [N] (n_frames,)
+            fz4 (npt.NDArray): Raw z-direction force acting on Kistler at sensor 4 [N] (n_frames,)
+            stance_thresh (float, optional): Vertical (z) force at which subject is in stance on Hexapod, when COP will be computed [N]. Defaults to 18.0.
 
         Returns:
             Dict[str, npt.NDArray]: Ground reaction force parameters, including:
-                'force' (npt.NDArray): Force components in global frame (n_frames,3)
-                'moment_origin' (npt.NDArray): Moment components in global frame represented at global origin (n_frames,3)
-                'COP' (npt.NDArray): Center of pressure coordinates in global frame (n_frames,3)
-                'moment_free' (npt.NDArray): Free moment (friction) components at COP in global frame (n_frames,3)
-                'moment_free_scalar' (float): Magnitude of free moment.
+                'force' (npt.NDArray): Force components in global frame [N] (n_frames,3)
+                'moment_origin' (npt.NDArray): Moment components in global frame represented at global origin [N*mm] (n_frames,3)
+                'COP' (npt.NDArray): Center of pressure coordinates in global frame [mm] (n_frames,3)
+                'moment_free' (npt.NDArray): Free moment (friction) components at COP in global frame [N*mm] (n_frames,3)
+                'moment_free_scalar' (float): Magnitude of free moment [N*mm].
         """
         
         assert cluster_hex.shape[0] == fz1.shape[0], f"Force data length ({fz1.shape[0]}) does not match marker data length ({cluster_hex.shape[0]})."
@@ -138,8 +138,8 @@ class HexKistler:
 
         Returns:
             Tuple[npt.NDArray, npt.NDArray]:
-                corners (npt.NDArray): Kistler corner trajectories (n_frames,4 corners,3)
-                sensors (npt.NDArray): Kistler sensor trajectories (n_frames,4 sensors,3)
+                corners (npt.NDArray): Kistler corner trajectories [mm] (n_frames,4 corners,3)
+                sensors (npt.NDArray): Kistler sensor trajectories [mm] (n_frames,4 sensors,3)
         """
 
         corners_K = np.array([ # surface corner locations (homogeneous) in Kistler's frame
@@ -168,9 +168,9 @@ class HexKistler:
 
         Returns:
             Optional[Dict[str, npt.NDArray]]: Rotational axis parameters, including:
-                'origin' (npt.NDArray): Axis reference point nearest to base/neutral Kistler origin (3,)
+                'origin' (npt.NDArray): Axis reference point nearest to base/neutral Kistler origin [mm] (3,)
                 'direction' (npt.NDArray): Unit vector axis of rotation (3,)
-                'angle' (float): Rotation magnitude (radians)
+                'angle' (float): Rotation magnitude [radians]
         """
 
         if len(cluster_hex_pert.shape) == 3: # frame dimension included (n_frames,n_markers,3)

src/hexapod_biomechanics/inverse_dynamics.py
@@ -13,11 +13,11 @@ class AnkleID:
         # inertial properties estimated according to Dumas et al 2006 (doi:10.1016/j.jbiomech.2006.02.013)
         assert sex in ["m", "f"], f"Invalid sex: {sex}. Choose \"m\" or \"f\"."
         if sex == "m":
-            self.m_F = 0.012 * body_mass # mass of foot (kg)
+            self.m_F = 0.012 * body_mass # mass of foot [kg]
         elif sex == "f":
             self.m_F = 0.010 * body_mass
         
-        self.I_F =  self.m_F * Inorm # foot inertia matrix in local foot (calcaneus) frame
+        self.I_F =  self.m_F * Inorm/1000.0 # foot inertia matrix in local foot (calcaneus) frame [kg*m*mm]
 
     def compute_dynamics(
             self,
@@ -34,8 +34,8 @@ class AnkleID:
             filt_poly: int = 4
     ) -> Dict[str, npt.ArrayLike]:
         
-        dt = np.mean(np.diff(t)) # sample period
-        fs = 1 / dt # sample frequency
+        dt = np.mean(np.diff(t)) # sample period [sec]
+        fs = 1 / dt # sample frequency [Hz]
 
         # filtered frequencies is tied to time-length of filter window
         # compute stable filter window according to time length and sample frequency
@@ -51,51 +51,51 @@ class AnkleID:
             axis=0
         )
 
-        x_ajc = filt(o_ajc, deriv=0) # anatomical joint center: intermalleolar point (n_frames,3)
-        omega_F, alpha_F = differentiate_rotation(t, R_F, filt_window, filt_poly) # foot rotations in local foot frame (n_frames,3)
+        x_ajc = filt(o_ajc, deriv=0) # anatomical joint center: intermalleolar point [mm] (n_frames,3)
+        omega_F, alpha_F = differentiate_rotation(t, R_F, filt_window, filt_poly) # foot rotations in local foot frame [rad/s] [rad/s/s] (n_frames,3)
         if COM_F is not None: # compute foot kinematics
-            x_com = filt(COM_F, deriv=0) # center of motion position (n_frames,3)
-            a_com = filt(COM_F, deriv=2) # center of motion acceleration (n_frames,3)
+            x_com = filt(COM_F, deriv=0) # center of motion position [mm] (n_frames,3)
+            a_com = filt(COM_F, deriv=2) # center of motion acceleration [mm/s/s] (n_frames,3)
 
-        F_grf = filt(grf_force, deriv=0)
-        M_grf_origin = filt(grf_moment_origin, deriv=0)
+        F_grf = filt(grf_force, deriv=0) # [N]
+        M_grf_origin = filt(grf_moment_origin, deriv=0) # [N*mm]
 
         # Force Balance
         # F_ank + F_grf + F_grav = m*a_com
         if COM_F is None: # simple: static with no gravity
-            F_ank = -F_grf
+            F_ank = -F_grf # [N]
         else:
-            F_grav = np.zeros_like(F_grf) + np.array([0, 0, self.m_F * -g]) # (n_frame,3)
-            F_ank = (self.m_F * a_com) - F_grf - F_grav # ankle interface forces (n_frames,3)
+            F_grav = np.zeros_like(F_grf) + np.array([0, 0, self.m_F * -g]) # [N] (n_frame,3)
+            F_ank = (self.m_F * a_com) - F_grf - F_grav # [N] ankle interface forces (n_frames,3)
 
 
         # Moment Balance: about ankle joint center
         # interface forces (F_ank) are coincident / not contributing
         # M_ank + M_grf_ajc + M_grav_ajc = M_I_com + (m_F * ((x_com - x_ajc) x a_com))
-        M_grf_ajc = M_grf_origin + np.cross(-x_ajc, F_grf) # ground reaction force moment represented at ankle joint center (n_frames,3)
+        M_grf_ajc = M_grf_origin + np.cross(-x_ajc, F_grf) # ground reaction force moment represented at ankle joint center [N*mm] + [mm]x[N] = [N*mm] (n_frames,3)
         if COM_F is None: # simple: static with no gravity
-            M_ank = -M_grf_ajc
+            M_ank = -M_grf_ajc # [N*mm]
             M_I_ajc = np.zeros_like(M_ank)
             M_grav_ajc = np.zeros_like(M_ank)
         else:
-            r_com_to_ajc = x_com - x_ajc # moment arm from ankle joint center to foot center of mass (n_frames,3)
-            M_grav_ajc = np.cross(r_com_to_ajc, F_grav) # moment due to gravity at ankle joint center (n_frames,3)
-            Ialpha = (self.I_F @ alpha_F[..., np.newaxis]).squeeze() # moment due to foot angular acceleration (3,3) @ (n_frames,3,1) -> (n_frames,3)
-            Iomega = (self.I_F @ omega_F[..., np.newaxis]).squeeze() # product of local inertia and local angular velocity (3,3) @ (n_frames,3,1) -> (n_frames,3)
-            M_I_local = Ialpha + np.cross(omega_F, Iomega) # moment due to COM inertia in local foot frame (n_frames,3)
-            M_I_com = (R_F @ M_I_local[..., np.newaxis]).squeeze() # moment due to COM inertia in global frame (n_frames,3)
-            M_I_ajc = M_I_com + np.cross(r_com_to_ajc, self.m_F * a_com) # moment due to inertia at ankle joint center (n_frames,3)
-            M_ank = M_I_ajc - M_grf_ajc - M_grav_ajc # ankle moments (n_frames,3)
+            r_com_to_ajc = x_com - x_ajc # moment arm from ankle joint center to foot center of mass [mm] (n_frames,3)
+            M_grav_ajc = np.cross(r_com_to_ajc, F_grav) # moment due to gravity at ankle joint center [N*mm] (n_frames,3)
+            Ialpha = (self.I_F @ alpha_F[..., np.newaxis]).squeeze() # moment due to foot angular acceleration [kg*m*mm]*[rad/s/s] = [kg*m*mm/s/s] = [N*mm] (3,3) @ (n_frames,3,1) -> (n_frames,3)
+            Iomega = (self.I_F @ omega_F[..., np.newaxis]).squeeze() # product of local inertia and local angular velocity [kg*m*mm]*[rad/s] = [kg*m*mm/s] (3,3) @ (n_frames,3,1) -> (n_frames,3)
+            M_I_local = Ialpha + np.cross(omega_F, Iomega) # moment due to COM inertia in local foot frame [N*mm] + [kg*m*mm/s]x[rad/s] = [N*mm] (n_frames,3)
+            M_I_com = (R_F @ M_I_local[..., np.newaxis]).squeeze() # moment due to COM inertia in global frame [N*mm] (n_frames,3)
+            M_I_ajc = M_I_com + np.cross(r_com_to_ajc, self.m_F * a_com/1000.0) # moment due to inertia at ankle joint center [N*mm] + [mm]x[kg]*[m/s/s] = [N*mm] (n_frames,3)
+            M_ank = M_I_ajc - M_grf_ajc - M_grav_ajc # ankle moments [N*mm] (n_frames,3)
 
         e1 = normalize(filt(e1, deriv=0)) # dorsiflexion/plantarflexion axis (n_frames,3)
         e2 = normalize(filt(e2, deriv=0)) # inversion/eversion axis (n_frames,3)
         e3 = normalize(filt(e3, deriv=0)) # internal/external rotation (n_frames,3)
 
-        M_e1 = np.sum(M_ank * e1, axis=-1) # (n_frames,)
+        M_e1 = np.sum(M_ank * e1, axis=-1) # [N*mm] (n_frames,)
         M_e2 = np.sum(M_ank * e2, axis=-1)
         M_e3 = np.sum(M_ank * e3, axis=-1)
 
-        F_e1 = np.sum(F_ank * e1, axis=-1) # (n_frames,)
+        F_e1 = np.sum(F_ank * e1, axis=-1) # [N*mm] (n_frames,)
         F_e2 = np.sum(F_ank * e2, axis=-1)
         F_e3 = np.sum(F_ank * e3, axis=-1)
 

src/hexapod_biomechanics/kinematics.py
@@ -33,14 +33,14 @@ class AnkleFrame:
 
         Args:
             side (int): Ankle side (1: right, -1: left)
-            MM_static (npt.NDArray): Medial malleolus across a static trial (n_frames, 3)
-            LM_static (npt.NDArray): Lateral malleolus across a static trial (n_frames, 3)
-            MC_static (npt.NDArray): Medial tibial condyle across a static trial (n_frames, 3)
-            LC_static (npt.NDArray): Lateral tibial condyle across a static trial (n_frames, 3)
-            CALC_static (npt.NDArray): Calcaneus (heel) across a static trial (n_frames,3)
-            M1_static (npt.NDArray): First metatarsal across a static trial (n_frames,3)
-            M5_static (npt.NDArray): Fifth metatarsal across a static trial (n_frames,3)
-            cluster_S_static (npt.NDArray): Shank cluster markers across a static trial (n_frames, n_markers, 3)
+            MM_static (npt.NDArray): Medial malleolus across a static trial [mm] (n_frames, 3)
+            LM_static (npt.NDArray): Lateral malleolus across a static trial [mm] (n_frames, 3)
+            MC_static (npt.NDArray): Medial tibial condyle across a static trial [mm] (n_frames, 3)
+            LC_static (npt.NDArray): Lateral tibial condyle across a static trial [mm] (n_frames, 3)
+            CALC_static (npt.NDArray): Calcaneus (heel) across a static trial [mm] (n_frames,3)
+            M1_static (npt.NDArray): First metatarsal across a static trial [mm] (n_frames,3)
+            M5_static (npt.NDArray): Fifth metatarsal across a static trial [mm] (n_frames,3)
+            cluster_S_static (npt.NDArray): Shank cluster markers across a static trial [mm] (n_frames, n_markers, 3)
             sex (npt.NDArray): Sex of subject ("m": male or "f": female) for applying anthropometric data. Defaults to "m".
         """
         
@@ -105,17 +105,17 @@ class AnkleFrame:
         if sex == "m":
             COM_SCS = np.array([0.382, -0.151, 0.026])*L_seg
             Inorm_SCS = np.real((np.array([
-                [17, 13, 8j],
-                [13, 37, 0],
-                [8j, 0, 36]
-            ]) * L_seg)**2) # segment-mass-normalized inertia matrix: r_ij = (1/L_seg)*sqrt(I_ij/m_seg)
+                [0.17, 0.13, 0.08j],
+                [0.13, 0.37, 0.0],
+                [0.08j, 0.0, 0.36]
+            ]) * L_seg)**2) # segment-mass-normalized inertia matrix [mm^2]: r_ij = (1/L_seg)*sqrt(I_ij/m_seg)
         elif sex == "f":
             COM_SCS = np.array([0.270, -0.218, 0.039])*L_seg
             Inorm_SCS = np.real((np.array([
-                [17, 10j, 6],
-                [10j, 36, 4j],
-                [6, 4j, 35]
-            ]) * L_seg)**2) # segment-mass-normalized inertia matrix: r_ij = (1/L_seg)*sqrt(I_ij/m_seg)
+                [0.17, 0.10j, 0.06],
+                [0.10j, 0.36, 0.04j],
+                [0.06, 0.04j, 0.35]
+            ]) * L_seg)**2) # segment-mass-normalized inertia matrix [mm^2]: r_ij = (1/L_seg)*sqrt(I_ij/m_seg)
         self.COM_C = (np.linalg.inv(self.T_CG_neut) @ T_SCS @ np.append(COM_SCS, 1))[:3] # foot COM represented in calcaneus frame (fixed to calc. frame) (3,)
         self.Inorm_C = R_SCS @ Inorm_SCS @ R_SCS.T # rotate inertia matrix to calcaneus frame (3,3)
 
@@ -145,25 +145,25 @@ class AnkleFrame:
         """Compute 6-DOF ankle joint kinematics relative to neutral static configuration.
 
         Args:
-            CALC (npt.NDArray): Calcaneus trajectory (n_frames,3)
-            M1 (npt.NDArray): First metatarsal trajectory (n_frames,3)
-            M5 (npt.NDArray): Fifth metatarsal trajectory (n_frames,3)
-            cluster_S (npt.NDArray): Shank cluster trajectory (n_frames,n_markers,3)
+            CALC (npt.NDArray): Calcaneus trajectory [mm] (n_frames,3)
+            M1 (npt.NDArray): First metatarsal trajectory [mm] (n_frames,3)
+            M5 (npt.NDArray): Fifth metatarsal trajectory [mm] (n_frames,3)
+            cluster_S (npt.NDArray): Shank cluster trajectory [mm] (n_frames,n_markers,3)
 
         Returns:
             Dict[str, npt.NDArray]: Kinematics including:
-                'alpha' (npt.NDArray): Dorsiflexion/plantarflexion angle (n_frames,)
-                'beta' (npt.NDArray): Inversion/eversion angle (n_frames,)
-                'gamma' (npt.NDArray): Internal/external rotation angle (n_frames,)
-                'q1' (npt.NDArray): Medial/lateral shift (n_frames,)
-                'q2' (npt.NDArray): Anterior/posterior shift (n_frames,)
-                'q3' (npt.NDArray): Vertical shift (n_frames,)
+                'alpha' (npt.NDArray): Dorsiflexion/plantarflexion angle [rad] (n_frames,)
+                'beta' (npt.NDArray): Inversion/eversion angle [rad] (n_frames,)
+                'gamma' (npt.NDArray): Internal/external rotation angle [rad] (n_frames,)
+                'q1' (npt.NDArray): Medial/lateral shift [mm] (n_frames,)
+                'q2' (npt.NDArray): Anterior/posterior shift [mm] (n_frames,)
+                'q3' (npt.NDArray): Vertical shift [mm] (n_frames,)
                 'e1' (npt.NDArray): Dorsiflexion/plantarflexion axis (n_frames,3)
                 'e2' (npt.NDArray): Inversion/eversion axis (n_frames,3)
                 'e3' (npt.NDArray): Internal/external rotation axis (n_frames,3)
-                'o_ajc' (npt.NDArray): Ankle joint center: intermalleolar point (tibia/fibula frame origin) (n_frames,3)
+                'o_ajc' (npt.NDArray): Ankle joint center: intermalleolar point (tibia/fibula frame origin) [mm] (n_frames,3)
                 'R_F' (npt.NDArray): Foot orientation (rotation matrix tracking calcaneus frame) (n_frames,3,3)
-                'COM_F (npt.NDArray): Foot center of mass in the global frame (n_frames,3)
+                'COM_F (npt.NDArray): Foot center of mass in the global frame [mm] (n_frames,3)
         """
 
         T_S_move = rigid_transform(self.cluster_S_neut, cluster_S) # shank movement from base to dynamic

src/hexapod_biomechanics/visualize.py
@@ -27,17 +27,17 @@ def animate_ankle_kinematics(
     """Creates animation of the ankle joint coordinate system with corresponding kinematics.
 
     Args:
-        t (npt.NDArray): Time vector (n_frames,)
-        alpha (npt.NDArray): Dorsiflexion/plantarflexion angle trajectory (n_frames,)
-        beta (npt.NDArray): Inversion/eversion angle trajectory (n_frames,)
-        gamma (npt.NDArray): Internal/external rotation angle trajectory (n_frames,)
+        t (npt.NDArray): Time vector [sec] (n_frames,)
+        alpha (npt.NDArray): Dorsiflexion/plantarflexion angle trajectory [rad] (n_frames,)
+        beta (npt.NDArray): Inversion/eversion angle trajectory [rad] (n_frames,)
+        gamma (npt.NDArray): Internal/external rotation angle trajectory [rad] (n_frames,)
         e1 (npt.NDArray): Dorsiflexion/plantarflexion ankle axis (n_frames,3)
         e2 (npt.NDArray): Inversion/eversion ankle axis (n_frames,3)
         e3 (npt.NDArray): Internal/external rotation ankle axis (n_frames,3)
-        T_T (npt.NDArray): Tibia/fibula frame representation (n_frames,4,4)
-        T_C (npt.NDArray): Calcaneus frame representation (n_frames,4,4)
-        COM_F (npt.NDArray): Foot center of mass (n_frames,3)
-        markers (npt.NDArray): Marker trajectories to include in animation (n_frames, n_markers, 3)
+        T_T (npt.NDArray): Tibia/fibula frame representation [mm] (n_frames,4,4)
+        T_C (npt.NDArray): Calcaneus frame representation [mm] (n_frames,4,4)
+        COM_F (npt.NDArray): Foot center of mass [mm] (n_frames,3)
+        markers (npt.NDArray): Marker trajectories to include in animation [mm] (n_frames, n_markers, 3)
         side (int): Ankle side (1: right, -1: left)
         speed (float, optional): Playback speed scale. Defaults to 1.0.
         animation_fps (int, optional): Animation frames per second. Defaults to 30.
@@ -54,7 +54,7 @@ def animate_ankle_kinematics(
     frame_step = fs_data / fs_animation
     ani_f_idx = np.round(np.arange(0, t.shape[0], frame_step)).astype(int)
 
-    alpha = np.degrees(alpha)
+    alpha = np.degrees(alpha) # [deg]
     beta = np.degrees(beta)
     gamma = np.degrees(gamma)
 
@@ -128,11 +128,11 @@ def animate_ankle_kinematics(
             """Helper for drawing 3D vectors.
 
             Args:
-                o (npt.NDArray): Vector origin (3,)
+                o (npt.NDArray): Vector origin [mm] (3,)
                 v (npt.NDArray): Vector (3,)
                 color (str): Plotting color.
                 lw (float, optional): Vector linewidth. Defaults to 2.
-                length (float, optional): Vector length. Defaults to 40.
+                length (float, optional): Vector length [mm]. Defaults to 40.
             """
 
             q = ax3d.quiver(
@@ -201,20 +201,20 @@ def animate_grf(
     """Creates animation of the ground reaction force in context with body and hexapod position.
 
     Args:
-        t (npt.NDArray): Time vector (n_frames,)
-        forces (npt.NDArray): Component forces acting on the subject in global frame (n_frames,3)
-        COP (npt.NDArray): Center of pressure coordinates in global frame (n_frames,3)
-        moment_free (npt.NDArray): Free moment (twist, frictional) at COP in global frame (n_frames,3)
-        corners (npt.NDArray): Coordinate trajectories of the Kistler corners (n_frames,n_corners,3)
-        sensors (npt.NDArray): Coordinate trajectories of the Kistler sensors (n_frames,n_sensors,3)
-        kist_origin_base (npt.NDArray): Origin of the Kistler coordinate system in the base/static configuration (3,)
+        t (npt.NDArray): Time vector [sec] (n_frames,)
+        forces (npt.NDArray): Component forces acting on the subject in global frame [N] (n_frames,3)
+        COP (npt.NDArray): Center of pressure coordinates in global frame [mm] (n_frames,3)
+        moment_free (npt.NDArray): Free moment (twist, frictional) at COP in global frame [N*mm] (n_frames,3)
+        corners (npt.NDArray): Coordinate trajectories of the Kistler corners [mm] (n_frames,n_corners,3)
+        sensors (npt.NDArray): Coordinate trajectories of the Kistler sensors [mm] (n_frames,n_sensors,3)
+        kist_origin_base (npt.NDArray): Origin of the Kistler coordinate system in the base/static configuration [mm] (3,)
         side (int): Ankle side (1: right, -1: left)
-        markers (npt.NDArray): Marker trajectories to incude in animation (n_frames,n_markers,3)
+        markers (npt.NDArray): Marker trajectories to incude in animation [mm] (n_frames,n_markers,3)
         speed (float, optional): Playback speed scale. Defaults to 1.0.
         animation_fps (int, optional): Animation frames per second. Defaults to 30.
-        stance_thresh (float, optional): Vertical (z) force at which subject is in stance on Hexapod, when COP/GRF will be plotted. Defaults to 18.0.
-        force_scale (float, optional): Length of force vector relative to magnitude, in mm/N. Defaults to 0.5.
-        moment_scale (float, optional): Length of free moment vector relative to magnitude, in mm/N. Defaults to 0.05.
+        stance_thresh (float, optional): Vertical (z) force at which subject is in stance on Hexapod, when COP/GRF will be plotted [N]. Defaults to 18.0.
+        force_scale (float, optional): Length of force vector relative to magnitude [mm/N]. Defaults to 0.5.
+        moment_scale (float, optional): Length of free moment vector relative to magnitude [mm/N]. Defaults to 0.05.
         filename (Optional[str], optional): Filename/path to save animation. Defaults to None.
 
     Returns:
@@ -415,9 +415,11 @@ def animate_perturbation(
         all_data = np.concatenate(line_datas, axis=0)
         rng = np.nanmax(all_data) - np.nanmax(all_data)
         ax.set_ylim(np.nanmin(all_data) - 0.1*rng, np.nanmax(all_data) + 0.1*rng)
+        labels = ['GRF', 'grav', 'inertia']
         colors = ['c', 'm', 'y']
         for i, comp in enumerate(M_components):
-            lines.append(ax_mom.plot([], [], c=colors[i], lw=1)[0])
+            lines.append(ax_mom.plot([], [], c=colors[i], lw=1, label=labels[i])[0])
+        ax_mom.legend(loc='upper right', fontsize='small')
 
     z_max = np.nanmax(markers[:, :, 2]) * 1.2
     z_min = -60